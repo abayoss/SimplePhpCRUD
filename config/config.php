09675a569c64586215a7662cd39ac2c70e587d64
abayoss <abayoss@yahoo.com>
2018-12-15 17:29:56 +0100

    root

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost/ForWeb/PHP/SimplePhpCRUD/website8/');
+	define('ROOT_URL', 'http://localhost/ForWeb/PHP/SimplePhpCRUD/');
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
